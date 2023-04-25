203185948d306abe92e7cbd4935f6ff488017850
Cristian Pan <a16003566@alumnos.uady.mx>
2023-04-24 23:10:04 -0600

    Seccion-15: completa

--- festivalMusica/gulpfile.js
+++ festivalMusica/gulpfile.js
@@ -1,59 +1,71 @@
 const { src, dest, watch, parallel } = require("gulp");
 
 //CSS
-const sass = require("gulp-sass")(require('sass'));
-const plumber = require('gulp-plumber'); 
+const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
 //Imagenes
-const avif = require('gulp-avif'); 
-const cache = require('gulp-cache'); 
-const imageMin = require('gulp-imagemin'); 
-const webp = require('gulp-webp'); 
-
+const avif = require("gulp-avif");
+const cache = require("gulp-cache");
+const imageMin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const terser = require("gulp-terser");
 
 function css(done) {
-  src("./src/scss/**/*.scss")  // Identificar el archivo de SASS
-  .pipe(plumber())
-  .pipe(sass())  // Compilarlo
-  .pipe(dest("css")); // Almacenarla en el disco duro
-
-  done(); //call back que avisa a gulp cuando se llega al final
+  src("src/scss/**/*.scss") //Identificar el archivo SASS
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(sass()) //Compilarlo
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("css")); //Almacenarlo en el disco duro
+  done(); //Callback que avisa a gulp cuando llegamos al final
 }
 
 function versionAvif(done) {
   const options = {
-    quality: 50
-  }; 
-  src('src/img/**/*.{png,jpg}')
-  .pipe(avif(options))
-  .pipe(dest('img'))
-  done(); 
+    quality: 50,
+  };
+  src("src/img/**/*.{png,jpg}").pipe(avif(options)).pipe(dest("img"));
+  done();
 }
 function versionWebp(done) {
   const options = {
-    quality: 50
-  }; 
-  src('src/img/**/*.{png,jpg}')
-  .pipe(webp(options))
-  .pipe(dest('img'))
-  done(); 
+    quality: 50,
+  };
+  src("src/img/**/*.{png,jpg}").pipe(webp(options)).pipe(dest("img"));
+  done();
 }
 
-function image (done) {
+function image(done) {
   const options = {
-    optmizitationLevel: 3
-  }; 
-  src('src/img/**/*.{png,jpg}')
-  .pipe(cache(imageMin(options)))
-  .pipe(dest('img'))
-  done(); 
+    optmizitationLevel: 3,
+  };
+  src("src/img/**/*.{png,jpg}")
+    .pipe(cache(imageMin(options)))
+    .pipe(dest("img"));
+  done();
 }
 
+function javascript(done) {
+  src("./src/js/**/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("js"));
+
+  done();
+}
 
 function dev(done) {
-    watch("./src/scss/**/*.scss", css); 
-    done();
+  watch("src/scss/**/*.scss", css);
+  watch("src/js/**/*.js", javascript);
+  done();
 }
-exports.versionAvif = versionAvif; 
-exports.image = image; 
-exports.versionWebp = versionWebp; 
-exports.css = parallel(image, versionWebp, versionAvif, dev);
+exports.versionAvif = versionAvif;
+exports.image = image;
+exports.versionWebp = versionWebp;
+exports.css = parallel(javascript, dev);
