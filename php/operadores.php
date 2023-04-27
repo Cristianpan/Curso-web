a7a6a0181f0f5667c2f90c5dc5488b37aa5907d1
Cristian Pan <a16003566@alumnos.uady.mx>
2023-04-26 18:15:56 -0600

    Seccion 24: videos del 254 al 258

---
+++ php/operadores.php
@@ -0,0 +1,22 @@
+<?php include 'includes/header.php';
+$num1 = 20; 
+$num2 = 10; 
+
+//Suma
+echo $num1 + $num2;
+echo "<br>"; 
+//Resta
+echo $num1 + $num2; 
+echo "<br>"; 
+//Multiplicar 
+echo $num1*$num2; 
+echo "<br>"; 
+//Divison
+echo $num1/$num2; 
+echo "<br>"; 
+//Potencia 
+echo $num1**2; 
+echo "<br>"; 
+
+
+include 'includes/footer.php';
---
+++ php/variables.php
@@ -0,0 +1,20 @@
+<?php include 'includes/header.php';
+
+//Funcionan como un let en js
+$nombre = "Cristian"; 
+$apellido = "Pan"; 
+
+echo "<p>$nombre $apellido</p>";
+
+//Constantes 
+define('constante', "Valor constante"); 
+
+const variable = "Hola Mundo"; 
+
+echo variable; 
+
+
+
+
+
+include 'includes/footer.php';
