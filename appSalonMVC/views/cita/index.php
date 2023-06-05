ef517a000cb097bb38e0e1f064889a772d0e35c8
Cristian Pan <a16003566@alumnos.uady.mx>
2023-06-05 14:24:43 -0600

    fixed warnings

--- appSalonMVC/views/cita/index.php
+++ appSalonMVC/views/cita/index.php
@@ -1,7 +1,7 @@
 <h1 class="nombre-pagina">Crear Nueva Cita</h1>
 <p class="descripcion-pagina">Elige tus servicios y coloca tus datos</p>
 
-<?php include_once __DIR__ . '/../components/barra.php.php'?>
+<?php include_once __DIR__ . '/../components/barra.php'?>
 
 <div id="app">
     <nav class="tabs">
